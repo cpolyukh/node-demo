78dd0a918bf31fccbc165b2151a00ecc23a3f08f
Christina Polyukh <cpolyukh@uw.edu>
2015-12-03 16:50:25 -0800

    class work

--- web-service/server.js
+++ web-service/server.js
@@ -8,6 +8,10 @@ var port = 8080;
 //to load all modules we need
 //express web server framework
 var express = require('express');
+//sqlite library
+var sqlite = require('sqlite3');
+//body parser library
+var bodyParser = require('body-parser');
 
 //create a new express app
 var app = express();
@@ -17,9 +21,71 @@ app.get('/', function(req, res) {
     res.send('<h1>Hello World!</h1>');
 });*/
 
+//tell express to server static files from the /static subdir
 app.use(express.static(__dirname + '/static'));
 
-//start the server
-app.listen(port, function() {
-    console.log('server is listening on http://localhost:' + port);
+//tell express to parse post body data as json
+app.use(bodyParser.json());
+
+app.get('/api/tasks', function(req, res, next) {
+    var sql = 'select rowid,title,done,createdOn from tasks where done != 1';
+    db.all(sql, function(err, rows) {
+        if (err) {
+            return next(err);
+        }
+
+        //send rows back to client as JSON
+        res.json(rows);
+    })
+});
+
+app.post('/api/tasks', function(req, res, next) {
+    var newTask = {
+        title: req.body.title,
+        done: false,
+        createdOn: new Date()
+    };
+    var sql = 'insert into tasks(title,done,createdOn) values(?,?,?)';
+    db.run(sql, [newTask.title, newTask.done, newTask.createdOn], function(err) {
+        if (err) {
+            return next(err);
+        }
+
+        res.status(201).json(newTask);
+    });
+});
+
+//when someone PUTs to /api/tasks/<task-id>...
+app.put('/api/tasks/:rowid', function(req, res, next) {
+    var sql = 'update tasks set done=? where rowid=?';
+    db.run(sql, [req.body.done, req.params.rowid], function(err) {
+        if (err) {
+            return next(err);
+        }
+
+        res.json(req.body);
+    });
+
+    //DON'T DO THIS!!
+    //var sql = 'update tasks set done=' + req.body;
+});
+
+//create database
+var db = new sqlite.Database(__dirname + '/data/tasks.db', function(err) {
+    if (err) {
+        throw err;
+    }
+
+    var sql = 'create table if not exists ' +
+        'tasks(title string, done int, createdOn datetime)';
+    db.run(sql, function(err) {
+        if (err) {
+            throw err;
+        }
+    });
+
+    //start the server
+    app.listen(port, function() {
+        console.log('server is listening on http://localhost:' + port);
+    });
 });
